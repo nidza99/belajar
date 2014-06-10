998ed9d770726f8a0ff5b476d3570dd2fac8e843
Andri Darmawan <andri.darmawan@gmail.com>
2014-06-10 11:08:05 +0700

    Create User.php

---
+++ Model/User.php
@@ -0,0 +1,88 @@
+<?php
+//App::uses('AppModel', 'Model');
+App::uses('AuthComponent', 'Controller/Component');
+/**
+ * User Model
+ *
+ */
+class User extends AppModel {
+
+
+	//---- ini tahapan password hashing, supaya password tidak plain text yang masuk ke db
+	public function beforeSave($options = array()) {
+		if (isset($this->data[$this->alias]['password'])) {
+			$this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
+			//$this->data['User']['password'] = AuthComponent::password($this->data['User']['password']);
+			}
+			return true;
+	}
+    //-------------------------------- end of passwd hashing
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'username' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				'message' => 'Your username was empty ',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'password' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'role' => array(
+			'valid' => array(
+				'rule' => array('inList', array('admin', 'author')),  
+				'message' => 'Please enter a valid role',
+				'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+						
+			),
+		),
+		'image' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),				
+	);
+	
+	
+	public $hasMany = array(
+		'Log' => array(
+			'className' => 'Log',
+			'foreignKey' => 'user_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);	
+	
+	
+}
